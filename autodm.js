7a1a5dde22154a7f25c60b0577ad8d66d518dbe4
Adebisi Ahmed <philip@MacBook-Pro.local>
2020-01-11 00:28:28 +0100

    initial commit

---
+++ autodm.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const T = require("./twit");
+const generateMessage = require("./message");
+const cron = require("node-cron");
+
+async function AutoDm() {
+  const text = await generateMessage();
+  const recipient_id = "369098139";
+  console.log(text);
+  // sendMessage({ text, recipient_id });
+}
+function sendMessage({ recipient_id, text }) {
+  T.post("direct_messages/events/new", {
+    event: {
+      type: "message_create",
+      message_create: {
+        target: {
+          recipient_id
+        },
+        message_data: {
+          text
+        }
+      }
+    }
+  })
+    .then(result => {
+      console.log(`Message sent successfully To recipient 💪💪`);
+    })
+    .catch(err => {
+      console.error("error", err);
+    });
+}
+
+cron.schedule("* 7 * * *", () => {
+  console.log("running  task every 7 am");
+  AutoDm();
+});
+// AutoDm();
---
+++ message.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer");
+
+async function fetchLoveMessage(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const [el] = await page.$x('//*[@id="random"]');
+  const textContent = await el.getProperty("textContent");
+  const loveMessage = await textContent.jsonValue();
+  browser.close();
+  return Promise.resolve(loveMessage);
+  // return loveMessage;
+}
+async function generateMessage() {
+  let message = await fetchLoveMessage(
+    "https://www.romanticlovemessages.com/random/"
+  );
+  while (message.includes("guy") || message.includes("man")) {
+    message = await fetchLoveMessage(
+      "https://www.romanticlovemessages.com/random/"
+    );
+  }
+  const index = message.lastIndexOf("Category");
+  const suffix = [
+    "LOYL 🌚",
+    "Your chargie 🧡",
+    "Baby Boy ❤️",
+    "Your King 👑",
+    "Your Honey Bunny 🍫",
+    "Baby Boo ❤️",
+    "❤️❤️❤️",
+    "Your Sunshine ☀️",
+    "Your Man 💕",
+    "Your Starboy 💥"
+  ];
+  const filteredMessage = `${message.slice(0, index)} - ${
+    suffix[Math.floor(Math.random() * suffix.length)]
+  }`;
+  return Promise.resolve(filteredMessage);
+}
+
+module.exports = generateMessage;
